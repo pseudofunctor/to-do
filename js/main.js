8381662a4060836d81bdf25deac079f9d7e4e70e
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2019-06-25 14:23:46 -0400

    adds read part of the app

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ const form = document.querySelector("form");
 const userEntry = document.getElementById("toDoItem");
 const ul = document.querySelector("ul");
 
+const items = [
+	{ name: "Eat an orange", complete: false },
+	{ name: "Go to the gym", complete: true }
+];
+
+items.forEach(item => {
+	let listItem = document.createElement("li");
+	listItem.innerHTML = item.name;
+	ul.appendChild(listItem);
+});
+
 form.addEventListener("submit", event => {
 	event.preventDefault();
 	let newEntry = document.createElement("li");
