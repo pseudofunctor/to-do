34a6100e1dd824c28ca1441f308d6f208168c359
ukarim <13909740+pseudofunctor@users.noreply.github.com>
2019-06-25 17:00:49 -0400

    completes strikethrough task

--- js/main.js
+++ js/main.js
@@ -8,15 +8,54 @@ const items = [
 	{ name: "Go to the gym", complete: true }
 ];
 
-items.forEach(item => {
-	let listItem = document.createElement("li");
-	listItem.innerHTML = item.name;
-	ul.appendChild(listItem);
+const addCompleteButtonListener = (completeButton, li, toDoItem) => {
+	completeButton.addEventListener("click", () => {
+		li.style.setProperty("text-decoration", "line-through");
+		toDoItem.complete = true;
+	});
+};
+
+items.forEach(toDoItem => {
+	let li = document.createElement("li");
+
+	let deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "x";
+
+	li.innerHTML = toDoItem.name + " ";
+	if (toDoItem.complete) {
+		li.style.setProperty("text-decoration", "line-through");
+	} else {
+		let completeButton = document.createElement("button");
+		completeButton.innerHTML = "v";
+		li.appendChild(completeButton);
+		addCompleteButtonListener(completeButton, li, toDoItem);
+	}
+
+	li.appendChild(deleteButton);
+	ul.appendChild(li);
 });
 
 form.addEventListener("submit", event => {
 	event.preventDefault();
 	let newEntry = document.createElement("li");
-	newEntry.innerHTML = userEntry.value;
+	
+	let completeButton = document.createElement("button");
+	completeButton.innerHTML = "v";
+
+	let deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "x";
+
+	newEntry.innerHTML = userEntry.value + " ";
+	newEntry.appendChild(completeButton);
+
+	let toDoItem = {
+		name: userEntry.value,
+		complete: false
+	};
+
+	addCompleteButtonListener(completeButton, newEntry, toDoItem);
+
+
+	newEntry.appendChild(deleteButton);
 	ul.appendChild(newEntry);
 });
